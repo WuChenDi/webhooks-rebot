f53bebc3eccc0bbe059cbd0d0af662b752651bed
WuChenDi <996194720@qq.com>
2022-05-12 18:04:16 +0800

    refactor: The whole project

--- app/utils/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 将所有相邻的空白字符转换为一个空格。
- *
- * 举例：collapseWhitespace('  String   \t libraries are   \n\n\t fun\n!  ') // 'String libraries are fun !'
- * @param {string} st 处理数据
- * @return {string} 处理好数据
- */
-export const collapseWhitespace = (st: string) => st.replace(/[\s\xa0]+/g, ' ')
-  .replace(/^\s+|\s+$/g, '');
